0d4113973470b155464eeeccd7779ec5f86269c2
jonathan gerber <jlgerber@gmail.com>
2018-12-21 15:10:56 -1000

    utils pylint 10

--- swinstall_stack/utils.py
+++ swinstall_stack/utils.py
@@ -1,18 +1,23 @@
-__all__ = ("datetime_from_str", "datetime_revision_from_str", "datetime_to_str")
+"""
+utils.py
+
+Utility functions for project.
+"""
 
 from datetime import datetime
-from .constants import DATETIME_FORMAT
 import logging
+from .constants import DATETIME_FORMAT
 
+__all__ = ("datetime_from_str", "datetime_revision_from_str", "datetime_to_str")
 
-log = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
 def datetime_from_str(datetime_str):
     """Given a string of the form YYYMMDD-HHMMSS, return a datetime instance.
 
     :param datetime_str: (str) representing a specific date and time
     :returns: datetime instance"""
-    return datetime.strptime(datetime_str,DATETIME_FORMAT)
+    return datetime.strptime(datetime_str, DATETIME_FORMAT)
 
 
 def datetime_revision_from_str(datetime_str):
